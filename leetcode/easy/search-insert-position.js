3416b2dbabd35f5c83490d624c8a681bc6aa81e0
Nam Nguyen <nam.nguyen@pointsbet.com>
2021-12-03 23:29:33 +1100

    leetcode: add one easy problem

---
+++ leetcode/easy/search-insert-position.js
@@ -0,0 +1,25 @@
+// Problem link: https://leetcode.com/problems/search-insert-position/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+};

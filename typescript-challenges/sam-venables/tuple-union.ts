303abe2ce0cac82084c09302665097a99c972d63
Nam Nguyen <nam.nguyen@pointsbet.com>
2023-11-17 17:48:38 +1100

    typescript-challenges: format comment

--- typescript-challenges/sam-venables/tuple-union.ts
+++ typescript-challenges/sam-venables/tuple-union.ts
@@ -52,17 +52,17 @@ export type Test2 = TupleUnion<["first", "second"], [2, 4, 6, 8]>; // ['first' |
 export type TupleToObject<T extends any[]> = Omit<T, keyof any[]>;
 
 export type ObjectToTuple<T> = T extends Record<number, any>
-  ? // if it's in a tuplable object format ({0: Type; 1: Type; 2: Type }), seed the recursion with an empty tuple
-    _BuildTuple<T, []>
+  // if it's in a tuplable object format ({0: Type; 1: Type; 2: Type }), seed the recursion with an empty tuple
+  ? _BuildTuple<T, []>
   : never;
 type _BuildTuple<
   T extends Record<number, any>,
   Result extends any[] = [],
 > = keyof T extends never
-  ? // when we've removed all of the keys from T, end the recursion
-    Result
-  : // if there are still keys left in T then pull the "next" element from T and spread into Result
-    _BuildTuple<
+  // when we've removed all of the keys from T, end the recursion
+  ? Result
+  // if there are still keys left in T then pull the "next" element from T and spread into Result
+  : _BuildTuple<
       // pass the source object, removing the key that matches the length of the current result
       Omit<T, `${Result["length"]}` | Result["length"]>,
       // pass the target tuple, adding the type that matches the key at the next position in the tuple
@@ -74,7 +74,9 @@ export type ObjectUnion<
   T2 extends Record<any, any>,
 > =
   // just the common keys
-  { [K in keyof (T1 | T2)]: (T1 | T2)[K] } & Partial< // combined with partials of the non-overlapping keys
+  { [K in keyof (T1 | T2)]: (T1 | T2)[K] }
+  // combined with partials of the non-overlapping keys
+  & Partial<
     Omit<T1, keyof T1 & keyof T2> & Omit<T2, keyof T1 & keyof T2>
   >;
 

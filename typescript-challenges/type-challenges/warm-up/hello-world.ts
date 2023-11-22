7ba052b5215cdae4e76410dc69cd065f7ef0cc15
Nam Nguyen <nguyenvietnam2401@gmail.com>
2023-11-23 01:00:25 +1100

    typescript-challenges(type-challenges): update hello-world

--- typescript-challenges/type-challenges/warm-up/hello-world.ts
+++ typescript-challenges/type-challenges/warm-up/hello-world.ts
@@ -33,7 +33,7 @@ type HelloWorld = string // expected to be a string
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect, NotAny } from '@type-challenges/utils'
 
-type cases = [
+export type cases = [
   Expect<NotAny<HelloWorld>>,
   Expect<Equal<HelloWorld, string>>,
 ]

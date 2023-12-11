d7bbfdc9b1dc9ffee4fa77339223a197db06321f
Nam Nguyen <nam.nguyen@pointsbet.com>
2023-12-12 10:37:31 +1100

    advent-of-code(js): update template

--- advent-of-code/js/read-entire-template/main.js
+++ advent-of-code/js/read-entire-template/main.js
@@ -15,3 +15,24 @@ function gcd(a, b) {
 function lcm(a, b) {
   return (a * b) / gcd(a, b);
 }
+
+var COLORS = {
+  RED: "\x1b[31m",
+  GREEN: "\x1b[32m",
+  YELLOW: "\x1b[33m",
+  BLUE: "\x1b[34m",
+  MAGENTA: "\x1b[35m",
+  CYAN: "\x1b[36m",
+  WHITE: "\x1b[37m",
+  GRAY: "\x1b[90m",
+  BLACK: "\x1b[30m",
+  RESET: "\x1b[0m",
+};
+function colorize(color, message) {
+  const template = `${COLORS[color]}%s${COLORS.RESET}`;
+  return { template, message };
+}
+function print(color, message) {
+  const colorized = colorize(color, message);
+  console.log(colorized.template, colorized.message);
+}

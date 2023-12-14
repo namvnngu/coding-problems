27d913fdc844780d6904cc2b4f22f412def8f788
Nam Nguyen <nam.nguyen@pointsbet.com>
2023-12-14 12:36:10 +1100

    advent-of-code(js): improve 2023/day-11/part-1

--- advent-of-code/js/2023/day-11/part-1/main.js
+++ advent-of-code/js/2023/day-11/part-1/main.js
@@ -38,14 +38,14 @@ function print(color, message) {
 const data = readFileSync("input.txt", "utf8").split("\n");
 data.pop();
 
+const EXTRA_STEP = 2;
+const GALAXY = "#";
+
 const galaxyLocations = (() => {
   const locations = [];
-  const graph = createGraph(data);
-
-  for (let r = 0; r < graph.length; r++) {
-    for (let c = 0; c < graph[0].length; c++) {
-      let node = graph[r][c];
-      if (node.value === "#") {
+  for (let r = 0; r < data.length; r++) {
+    for (let c = 0; c < data[0].length; c++) {
+      if (data[r][c] === GALAXY) {
         locations.push({ row: r, col: c });
       }
     }
@@ -53,120 +53,86 @@ const galaxyLocations = (() => {
   return locations;
 })();
 
-const destinations = [];
+const emptySpaces = (() => {
+  const emptySpaces = { rows: {}, cols: {} };
 
-for (let g = 0; g < galaxyLocations.length - 1; g++) {
-  const destLocations = galaxyLocations.slice(g + 1);
-  const startLocation = galaxyLocations[g];
-  const destNodes = [];
+  for (let r = 0; r < data.length; r++) {
+    if (!data[r].includes(GALAXY)) {
+      emptySpaces.rows[r] = r;
+    }
+  }
 
-  const graph = createGraph(data);
-  const startNode = graph[startLocation.row][startLocation.col];
-  const queue = [startNode];
-  startNode.visited = true;
+  for (let c = 0; c < data[0].length; c++) {
+    let isColEmpty = true;
+
+    for (let r = 0; r < data.length; r++) {
+      if (data[r][c] === GALAXY) {
+        isColEmpty = false;
+      }
+    }
+
+    if (isColEmpty) {
+      emptySpaces.cols[c] = c;
+    }
+  }
+
+  return emptySpaces;
+})();
+
+let sumShortestDist = 0;
+
+for (let g = 0; g < galaxyLocations.length; g++) {
+  const startSpaceLocation = galaxyLocations[g];
+  const universe = createUniverse(data);
+  const queue = [startSpaceLocation];
+  universe[startSpaceLocation.row][startSpaceLocation.col] = 0;
 
   while (queue.length !== 0) {
-    const parent = queue.shift();
-    for (const child of getChildren(parent, graph)) {
-      if (child.visited) continue;
+    const current = queue.shift();
+    for (const next of getNextLocations(current)) {
+      if (
+        !universe[next.row]?.[next.col] ||
+        typeof universe[next.row][next.col] === "number"
+      )
+        continue;
 
-      const found = destLocations.find(
-        (l) => l.col === child.col && l.row === child.row,
-      );
-      if (found) {
-        destNodes.push(child);
+      const nextStepCount = (() => {
+        if (
+          typeof emptySpaces.rows[current.row] === "number" ||
+          typeof emptySpaces.cols[current.col] === "number"
+        ) {
+          return universe[current.row][current.col] + EXTRA_STEP;
+        }
+        if (
+          (typeof emptySpaces.rows[current.row] === "number" &&
+            typeof emptySpaces.cols[current.col]) === "number"
+        ) {
+          return universe[current.row][current.col] + EXTRA_STEP * 2;
+        }
+        return universe[current.row][current.col] + 1;
+      })();
+
+      if (universe[next.row][next.col] === GALAXY) {
+        sumShortestDist += nextStepCount;
       }
 
-      child.visited = true;
-      child.parent = parent;
-      child.distance = parent.distance + 1;
-      queue.push(child);
+      universe[next.row][next.col] = nextStepCount;
+      queue.push(next);
     }
-  }
-
-  destinations.push(destNodes);
-}
-let result = 0;
-
-for (const destination of destinations) {
-  for (const node of destination) {
-    result += node.distance;
   }
 }
 
-console.log("Result:", result);
+console.log("Result:", sumShortestDist / 2);
 
-function createNode(value, row, col) {
-  return {
-    value,
-    col,
-    row,
-    distance: 0,
-    visited: false,
-    parent: undefined,
-  };
+function getNextLocations({ row, col }) {
+  return [
+    { row: row - 1, col },
+    { row: row + 1, col },
+    { row, col: col - 1 },
+    { row, col: col + 1 },
+  ];
 }
 
-function createGraph(data) {
-  const tempGraph1 = [];
-  for (let i = 0; i < data.length; i++) {
-    const r = data[i];
-    if (r.includes("#")) {
-      tempGraph1.push(r);
-    } else {
-      tempGraph1.push(r);
-      tempGraph1.push(r);
-    }
-  }
-
-  const tempGraph2 = [];
-  for (let c = 0; c < tempGraph1[0].length; c++) {
-    let isEmpty = true;
-
-    for (let r = 0; r < tempGraph1.length; r++) {
-      let value = tempGraph1[r][c];
-
-      tempGraph2[r] ??= "";
-      tempGraph2[r] += value;
-
-      if (value === "#") {
-        isEmpty = false;
-      }
-    }
-
-    if (isEmpty) {
-      for (let r = 0; r < tempGraph1.length; r++) {
-        tempGraph2[r] += ".";
-      }
-    }
-  }
-
-  const graph = [];
-  for (let r = 0; r < tempGraph2.length; r++) {
-    const row = [];
-    for (let c = 0; c < tempGraph2[0].length; c++) {
-      row.push(createNode(tempGraph2[r][c], r, c));
-    }
-    graph.push(row);
-  }
-
-  return graph;
-}
-
-function getChildren(parent, graph) {
-  const children = [];
-
-  const top = graph[parent.row - 1]?.[parent.col];
-  top && children.push(top);
-
-  const down = graph[parent.row + 1]?.[parent.col];
-  down && children.push(down);
-
-  const left = graph[parent.row]?.[parent.col - 1];
-  left && children.push(left);
-
-  const right = graph[parent.row]?.[parent.col + 1];
-  right && children.push(right);
-
-  return children;
+function createUniverse(data) {
+  return data.map((x) => x.split(""));
 }
